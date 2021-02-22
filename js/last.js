3940ffba49104e01d67fc62559155dd5d8756078
unknown <you@example.com>
2021-02-22 19:35:37 +0800

    final

--- js/last.js
+++ js/last.js
@@ -8,3 +8,12 @@ myprofile.addEventListener('click', () => {
 signout.addEventListener('click', ()=> {
     signout.classList.remove('active')
 })
+
+
+const studentBox = document.querySelectorAll('.student')
+
+studentBox.forEach( i => {
+    i.addEventListener('click' , () => {
+        window.location.href = '../teacher/studentinfo.html'
+    } )
+})
